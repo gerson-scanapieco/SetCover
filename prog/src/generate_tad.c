069dadc73498baf12172d0b7954d6d74668c1bd8
otavioarj <otavioarj@gmail.com>
2011-06-16 18:35:21 -0300

    intersection of groups

--- prog/src/generate_tad.c
+++ prog/src/generate_tad.c
@@ -4,8 +4,9 @@
 utilzada para ambos algoritmos
     @status Testada e funcionando
 */
-void getSUBSET(SUBSET *s){
+SUBSET getSUBSET(){
 
+    SUBSET s;
     int alphabet_size;
     int item;
     int qt_subset;
@@ -13,27 +14,28 @@ void getSUBSET(SUBSET *s){
     int i;
     int j;
     scanf("%d",&alphabet_size);
-    s->alphabet = (int *) malloc(alphabet_size*sizeof(int));
-    s->size_alphabet = alphabet_size;
+    s.alphabet = (int *) malloc(alphabet_size*sizeof(int));
+    s.size_alphabet = alphabet_size;
     for(i=0;i<alphabet_size;i++){
         scanf("%d",&item);
-        s->alphabet[i] = item;
+        s.alphabet[i] = item;
     }
     scanf("%d",&qt_subset);
-    s->subsets = (int **) malloc(qt_subset*sizeof(int *)); //define a qt de subconjuntos
-    s->qt_subsets = qt_subset;
+    s.subsets = (int **) malloc(qt_subset*sizeof(int *)); //define a qt de subconjuntos
+    s.qt_subsets = qt_subset;
     for(i=0;i<qt_subset;i++){
         /*para cada um dos subconjuntos ele le primeiro o tamanho do subconjunto
           e logo apos o programa aloca o tamanho do subconjunto e preenche com os
           valores da entrada padrao*/
         scanf("%d",&size_subset);
-        s->subsets[i] = (int *) malloc((size_subset+1)*sizeof(int));
-        s->subsets[i][0] = size_subset; //grava o tamanho do subconjunto na primeira posicao
+        s.subsets[i] = (int *) malloc((size_subset+1)*sizeof(int));
+        s.subsets[i][0] = size_subset; //grava o tamanho do subconjunto na primeira posicao
         for(j=1;j<=size_subset;j++){
             scanf("%d",&item);
-            s->subsets[i][j] = item;
+            s.subsets[i][j] = item;
         }
     }
+    return s;
 }
 
 
