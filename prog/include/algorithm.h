eea2f2eb1e598e5fef1f5d1539c214f1423f9c3a
Philippe Moneda <philippemoneda@gmail.com>
2011-06-16 11:17:52 -0300

    Criando arquivos do algoritmos

---
+++ prog/include/algorithm.h
@@ -0,0 +1,9 @@
+#ifndef ALGORITHM_H_INCLUDED
+#define ALGORITHM_H_INCLUDED
+
+#include "generate_tad.h"
+
+void brute_force_sc(SUBSET s);
+void greedy_sc(SUBSET s);
+
+#endif // ALGORITHM_H_INCLUDED
---
+++ prog/src/algorithm.c
@@ -0,0 +1,8 @@
+#include "../include/algorithm.h"
+
+void brute_force_sc(SUBSET s){
+    //TODO implementar algoritmo forca bruta
+}
+void greedy_sc(SUBSET s){
+    //TODO implementar algoritmo aproximado
+}
