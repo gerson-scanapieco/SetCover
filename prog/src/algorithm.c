e7602ea7f79ffce5b2bb61c36a4739d242a04fe2
Otavio Augusto <octopos8@gmail.com>
2011-06-16 14:46:58 -0300

    getSUBSET param ajustado;+intersectons of sets

--- prog/src/algorithm.c
+++ prog/src/algorithm.c
@@ -1,10 +1,21 @@
 #include "../include/algorithm.h"
 
-void brute_force_sc(SUBSET s){
-    //TODO implementar algoritmo forca bruta
+/* intersection of sets 
+ * subsets DEVEM estar ordenados!!
+ * set1 e set2 são 2 ints que relacionam a posição dos conjuntos no universo */
+void  isec(SUBSET *s, int set1, int set2,int *	intsec) {
+	short int i=0;
+  for(;i<s->size_alphabet;++i)		
+		if(s->alphabet[i]!=s->subsets[set1][i])
+			if(s->alphabet[i]!=s->subsets[set2][i])
+				intsec[i]=s->alphabet[i];		
+}
+
+void brute_force_sc(SUBSET *s){
+		// checa o universo em relação aos 2 conj ordenados
 
 }
-void greedy_sc(SUBSET s){
+void greedy_sc(SUBSET *s){
     //TODO implementar algoritmo aproximado
 
 }
--- prog/src/generate_tad.c
+++ prog/src/generate_tad.c
@@ -4,9 +4,8 @@
 utilzada para ambos algoritmos
     @status Testada e funcionando
 */
-SUBSET getSUBSET(){
+void getSUBSET(SUBSET *s){
 
-    SUBSET s;
     int alphabet_size;
     int item;
     int qt_subset;
@@ -14,28 +13,27 @@ SUBSET getSUBSET(){
     int i;
     int j;
     scanf("%d",&alphabet_size);
-    s.alphabet = (int *) malloc(alphabet_size*sizeof(int));
-    s.size_alphabet = alphabet_size;
+    s->alphabet = (int *) malloc(alphabet_size*sizeof(int));
+    s->size_alphabet = alphabet_size;
     for(i=0;i<alphabet_size;i++){
         scanf("%d",&item);
-        s.alphabet[i] = item;
+        s->alphabet[i] = item;
     }
     scanf("%d",&qt_subset);
-    s.subsets = (int **) malloc(qt_subset*sizeof(int *)); //define a qt de subconjuntos
-    s.qt_subsets = qt_subset;
+    s->subsets = (int **) malloc(qt_subset*sizeof(int *)); //define a qt de subconjuntos
+    s->qt_subsets = qt_subset;
     for(i=0;i<qt_subset;i++){
         /*para cada um dos subconjuntos ele le primeiro o tamanho do subconjunto
           e logo apos o programa aloca o tamanho do subconjunto e preenche com os
           valores da entrada padrao*/
         scanf("%d",&size_subset);
-        s.subsets[i] = (int *) malloc((size_subset+1)*sizeof(int));
-        s.subsets[i][0] = size_subset; //grava o tamanho do subconjunto na primeira posicao
+        s->subsets[i] = (int *) malloc((size_subset+1)*sizeof(int));
+        s->subsets[i][0] = size_subset; //grava o tamanho do subconjunto na primeira posicao
         for(j=1;j<=size_subset;j++){
             scanf("%d",&item);
-            s.subsets[i][j] = item;
+            s->subsets[i][j] = item;
         }
     }
-    return s;
 }
 
 
