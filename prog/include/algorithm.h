874ce6163856075123c2c6e7a6ae3bcf2ea40645
Philippe Moneda <philippemoneda@gmail.com>
2011-06-17 11:02:21 -0300

    sincornizando codigo com as alteracoes do otavio

--- prog/include/algorithm.h
+++ prog/include/algorithm.h
@@ -3,8 +3,8 @@
 
 #include "generate_tad.h"
 
-void brute_force_sc(SUBSET s);
-void greedy_sc(SUBSET s);
+void brute_force_sc(SUBSET *s);
+void greedy_sc(SUBSET *s);
 int  isec(SUBSET *s, int * set1, int *set2,int *	intsec);
 
 #endif // ALGORITHM_H_INCLUDED
